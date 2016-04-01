31104b2133bfb6a4ad9b12f3b039bc78229e44b9
a-mackay <opensauceburner@icloud.com>
2016-04-01 19:46:37 +1100

    Add intervention type class skeleton

---
+++ InterventionManagement.Core/InterventionType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Core
+{
+    class InterventionType
+    {
+        public String Name { get; set; }
+        public double Hours { get; set; }
+        public double Cost { get; set; }
+
+        public InterventionType(string name, double hours, double cost)
+        {
+            Name = name;
+            Hours = hours;
+            Cost = cost;
+        }
+    }
+}
