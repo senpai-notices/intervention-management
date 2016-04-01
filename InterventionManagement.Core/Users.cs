48ae8c668c75dc8449727f454e074ad54e7fbeff
Alex Tan <alext@outlook.com.au>
2016-04-01 20:30:19 +1100

    Created Users class

---
+++ InterventionManagement.Core/Users.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Core
+{
+    public class Users
+    {
+        private readonly List<User> _users = new List<User>();
+
+
+        // public List<User> Users { get { return _users; } }
+        // List cannot be same name as enclosing class
+        // So I will make a manual getter instead.
+
+        public List<User> GetUsers()
+        {
+            return _users;
+        }
+
+        public void Add(User user)
+        {
+            _users.Add(user);
+        }
+
+        public void Login(string username, string password)
+        {
+            
+        }
+
+    }
+}
