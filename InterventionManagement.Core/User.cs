5620231919633abffa87778feec2582231f59581
Alex Tan <alext@outlook.com.au>
2016-04-01 13:22:31 +1100

    Initial import

---
+++ InterventionManagement.Core/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Core
+{
+    public abstract class User
+    {
+        public int UserID { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Name { get; private set; }
+
+        protected User(int userId, string username, string password, string name)
+        {
+            UserID = userId;
+            Username = username;
+            Password = password;
+            Name = name;
+        }
+    }
+
+    public class Accountant : User
+    {
+        public Accountant(int userId, string username, string password, string name) 
+            : base(userId, username, password, name)
+        {
+        }
+    }
+
+    public class DistrictStaff : User
+    {
+        public double HoursApprovalLimit { get; set; }
+        public double CostApprovalLimit { get; set; }
+        public DistrictName District { get; set; }
+
+        public DistrictStaff(int userId, string username, string password, string name, 
+            double hoursApprovalLimit, double costApprovalLimit, DistrictName district) 
+            : base(userId, username, password, name)
+        {
+            HoursApprovalLimit = hoursApprovalLimit;
+            CostApprovalLimit = costApprovalLimit;
+            District = district;
+        }
+    }
+
+    public class Manager : DistrictStaff
+    {
+        public Manager(int userId, string username, string password, string name, 
+            double hoursApprovalLimit, double costApprovalLimit, DistrictName district) 
+            : base(userId, username, password, name, hoursApprovalLimit, costApprovalLimit, 
+                  district)
+        {
+        }
+    }
+
+    public class Engineer : DistrictStaff
+    {
+        public Engineer(int userId, string username, string password, string name, 
+            double hoursApprovalLimit, double costApprovalLimit, DistrictName district) 
+            : base(userId, username, password, name, hoursApprovalLimit, costApprovalLimit, 
+                  district)
+        {
+        }
+    }
+}
---
+++ InterventionManagement.Tests/UserTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreateNewAccountant()
+        {
+            var sut = new Accountant(1, "alextan", "123456", "Alex Tan");
+
+            Assert.AreEqual(sut.UserID, 1);
+            Assert.AreEqual(sut.Username, "alextan");
+            Assert.AreEqual(sut.Password, "123456");
+            Assert.AreEqual(sut.Name, "Alex Tan");
+        }
+
+        [TestMethod]
+        public void CreateNewManager()
+        {
+            var sut = new Manager(1, "manager", "123456", "John Smith", 10, 2000, DistrictName.Sydney);
+            Assert.AreEqual(sut.UserID, 1);
+            Assert.AreEqual(sut.Username, "manager");
+            Assert.AreEqual(sut.Password, "123456");
+            Assert.AreEqual(sut.Name, "John Smith");
+            Assert.AreEqual(sut.HoursApprovalLimit, 10);
+            Assert.AreEqual(sut.CostApprovalLimit, 2000);
+            Assert.AreEqual(sut.District, DistrictName.Sydney);
+        }
+
+
+
+        [TestMethod]
+        public void CreateNewSiteEngineer()
+        {
+            var sut = new Engineer(1, "steven", "testpassword", "Steven", 18.5, 5000, DistrictName.Sydney);
+            Assert.AreEqual(sut.UserID,1);
+            Assert.AreEqual(sut.Username, "steven");
+            Assert.AreEqual(sut.Password, "testpassword");
+            Assert.AreEqual(sut.Name, "Steven");
+            Assert.AreEqual(sut.HoursApprovalLimit, 18.5);
+            Assert.AreEqual(sut.CostApprovalLimit, 5000);
+            Assert.AreEqual(sut.District, DistrictName.Sydney);
+
+        }
+
+
+
+    }
+}
