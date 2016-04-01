3b2f62f6339fb7b2e36ecdffae848aded2434e85
Alex Tan <alext@outlook.com.au>
2016-04-01 20:30:30 +1100

    Created LoginTests

---
+++ InterventionManagement.Tests/LoginTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    class LoginTests
+    {
+        [TestMethod]
+        public void LoginWithCorrectDetails()
+        {
+            var sut = new Users();
+
+            sut.Add(new Accountant());
+            sut.Add(new Accountant());
+            sut.Add(new );
+            sut.Add(new );
+            sut.Add(new );
+            sut.Add(new );
+            sut.Add(new );
+            sut.Add(new );
+        }
+        
+
+    }
+}
