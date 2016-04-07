c143ecac8f3797aa79130e18649c2a926d32da0b
Alex Tan <alext@outlook.com.au>
2016-04-07 18:20:57 +1000

    ClientManager created.

---
+++ InterventionManagement.Core/ClientManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Core
+{
+    // Should this be static?
+    public static class ClientManager
+    {
+        private static readonly List<Client> _clients = new List<Client>();
+
+        public static List<Client> Clients => _clients;
+
+        public static void Add(Client client)
+        {
+            _clients.Add(client);
+        }
+    }
+}
