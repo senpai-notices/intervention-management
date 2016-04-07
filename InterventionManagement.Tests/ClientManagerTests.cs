05a8cbcb6c0f9b7727702eb27e364c424a08d706
Alex Tan <alext@outlook.com.au>
2016-04-07 18:21:36 +1000

    ClientManagerTests created

---
+++ InterventionManagement.Tests/ClientManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    [Ignore]
+    [TestClass]
+    public class ClientManagerTests
+    {
+        [TestMethod]
+        public void Foo()
+        { }
+    }
+}
