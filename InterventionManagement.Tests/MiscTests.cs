cfaa50e75f029b32d8966efa31ffe98f30209da4
Alex Tan <alext@outlook.com.au>
2016-04-07 16:12:31 +1000

    Added a test class for exploratory testing (set to ignore)

---
+++ InterventionManagement.Tests/MiscTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    [Ignore]
+    [TestClass]
+    public class MiscTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var rgx = new Regex("/([a-z'-]+) ([a-z'-]+)/i");
+            var rgx2 = new Regex("/^[a - z,.'-]+$/i");
+
+            var rgx10 = new Regex("[a-z'-]+");
+
+            StringAssert.Matches("alex1??", rgx10);
+        }
+    }
+}
