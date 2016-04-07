bf50e422bc6984fbf7b69eaad50d0a6bb2b45893
Alex Tan <alext@outlook.com.au>
2016-04-08 01:06:53 +1000

    InterventionManager created

---
+++ InterventionManagement.Core/InterventionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Core
+{
+    // Should this be static?
+    public static class InterventionManager
+    {
+        private static readonly List<Intervention> _interventions = new List<Intervention>();
+
+        public static List<Intervention> Interventions => _interventions;
+
+        public static void Add(Intervention intervention)
+        {
+            _interventions.Add(intervention);
+        }
+    }
+}
