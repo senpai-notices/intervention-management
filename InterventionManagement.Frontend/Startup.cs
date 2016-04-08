688ac369150d63387efac7d6c2da0f484ecae281
Alex Tan <alext@outlook.com.au>
2016-04-08 13:13:50 +1000

    ASP.NET Web Forms project created

---
+++ InterventionManagement.Frontend/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterventionManagement.Frontend.Startup))]
+namespace InterventionManagement.Frontend
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
