4d963699667a529dd38af53fa4c2beebaf6dba1b
Alex Tan <alext@outlook.com.au>
2016-04-08 13:30:06 +1000

    Refactored Intervention

--- InterventionManagement.Core/Intervention.cs
+++ InterventionManagement.Core/Intervention.cs
@@ -11,24 +11,13 @@ namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Core
 
         public List<QualityReport> QualityReport { get; private set; }
         
-        // Unsure whether to represent FKs as IDs or instances
-        public int InterventionTypeId { get; private set; }
+        //public int InterventionTypeId { get; private set; }
+        public string InterventionName { get; set; }
+        public decimal InterventionHours { get; set; }
+        public decimal InterventionCost { get; set; }
+
         public int ProposerId { get; private set; }
         public int ApproverId { get; private set; }
         public int ClientId { get; private set; }
-
-        public Intervention(int interventionId, DateTime datePerformed, InterventionState state, 
-            List<QualityReport> qualityReport, int interventionTypeId, int proposerId, 
-            int approverId, int clientId)
-        {
-            InterventionId = interventionId;
-            DatePerformed = datePerformed;
-            State = state;
-            QualityReport = qualityReport;
-            InterventionTypeId = interventionTypeId;
-            ProposerId = proposerId;
-            ApproverId = approverId;
-            ClientId = clientId;
-        }
     }
 }
