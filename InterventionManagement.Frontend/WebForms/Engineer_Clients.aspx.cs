f00b42d475c8140823daf98bddc0aca20a4a72e8
John Fox <J.Arcoudis@gmail.com>
2016-04-10 21:23:55 +1000

    Create Engineer_Clients Webform basic UI.

---
+++ InterventionManagement.Frontend/WebForms/Engineer_Clients.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InterventionManagement.Frontend.WebForms
+{
+    public partial class Engineer_Clients : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_ViewClient_Click(object sender, EventArgs e)
+        {
+            if(list_Clients.SelectedItem != null)
+            {
+
+            }
+        }
+
+        protected void btn_NewClient_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+    }
+}
