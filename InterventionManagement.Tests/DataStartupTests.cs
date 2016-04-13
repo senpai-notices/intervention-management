b9d2bfc9e547fea0872f04ccb7511a85b8bd0d2c
Alex Tan <alext@outlook.com.au>
2016-04-13 13:58:29 +1000

    Added DataStartup tests

---
+++ InterventionManagement.Tests/DataStartupTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    [TestClass]
+    class DataStartupTests
+    {
+        [AssemblyInitialize]
+        public static void SetupDataDirectory(TestContext context)
+        {
+            string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\UTS.Multitier.Data"));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
+
+        [TestMethod]
+        public void Connection_OpenClose_Succeeds()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            conn.Close();
+        }
+    }
+}
