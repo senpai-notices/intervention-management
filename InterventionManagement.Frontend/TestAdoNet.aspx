20f74eb780996150b825f37e26ea45568640d47d
Alex Tan <alext@outlook.com.au>
2016-04-16 14:05:29 +1000

    Add test page for DB ADO.NET connection

---
+++ InterventionManagement.Frontend/TestAdoNet.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestAdoNet.aspx.cs" Inherits="au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.TestAdoNet" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ InterventionManagement.Frontend/TestAdoNet.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web
+{
+    public partial class TestAdoNet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // string connString = DatabaseHelper.ConnectionString;
+
+            DatabaseHelper.ApplicationName = "New name";
+            DatabaseHelper.ConnectionTimeout = 42;
+            var conn = DatabaseHelper.GetSqlConnection();
+
+        }
+    }
+}
