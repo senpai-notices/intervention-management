3a1a4bbf61ecf66cbd26f032972e39e333989607
Alex Tan <alext@outlook.com.au>
2016-04-18 10:57:58 +1000

    Add repository skeleton

---
+++ InterventionManagement.Data/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Data.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
