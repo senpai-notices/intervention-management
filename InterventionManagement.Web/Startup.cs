b26a17c107bc10f6d3c393ff7d95f22f5fbc7547
a-mackay <11742400@outlook.com>
2016-04-27 17:04:26 +1000

    Change connection string to explicitly refer to DataDirectory

--- InterventionManagement.Web/Startup.cs
+++ InterventionManagement.Web/Startup.cs
@@ -8,13 +8,13 @@ using Owin;
 namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web
 {
     public partial class Startup {
-        public void Configuration(IAppBuilder app) {
+        public void Configuration(IAppBuilder app)
+        {
             ConfigureAuth(app);
 
-            // Database
+            // Set DataDirectory for the connection string in Web.config
             string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\InterventionManagement.Data"));
             AppDomain.CurrentDomain.SetData("DataDirectory", path);
-
         }
     }
 }
