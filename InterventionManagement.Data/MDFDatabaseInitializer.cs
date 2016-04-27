f15cc188781081d37a7d2fd0eef424b92831302f
a-mackay <11742400@outlook.com>
2016-04-27 18:24:39 +1000

    Add DbUp database setup functionality

---
+++ InterventionManagement.Data/MDFDatabaseInitializer.cs
@@ -0,0 +1,24 @@
+﻿using DbUp;
+using System.Reflection;
+using System.Configuration;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Data
+{
+    public class MDFDatabaseInitializer
+    {
+        public void InitializeDatabaseUsingDbUp()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            // DbUp methods to initialise the database using embedded SQL scripts
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+        }
+    }
+}
--- InterventionManagement.Web/Startup.cs
+++ InterventionManagement.Web/Startup.cs
@@ -3,6 +3,7 @@ using System.IO;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Web;
 using Microsoft.Owin;
 using Owin;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data;//remove
 
 [assembly: OwinStartup(typeof(Startup))]
 namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web
@@ -15,6 +16,9 @@ namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web
             // Set DataDirectory for the connection string in Web.config
             string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\InterventionManagement.Data"));
             AppDomain.CurrentDomain.SetData("DataDirectory", path);
+
+            // Debug to test db initializer
+            new MDFDatabaseInitializer().InitializeDatabaseUsingDbUp();
         }
     }
 }
