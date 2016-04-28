0394742cb0043e772592a89bfa2b6208c47b24cb
John Fox <J.Arcoudis@gmail.com>
2016-04-28 19:45:44 +1000

    Fiddling with SQL Scripts

---
+++ InterventionManagement.Data/tmpSQL/Queries.sql
@@ -0,0 +1,18 @@
+﻿
+-- Total Cost Of Pending Interventions Per Employee(Engineer)
+SELECT		i.ProposerId, SUM(i.Cost)
+FROM		Intervention i
+LEFT JOIN	InterventionState istate
+ON			i.InterventionStateId = istate.InterventionStateId
+WHERE		InterventionStateName = 'Proposed'
+GROUP BY	i.ProposerId;
+
+-- All Interventions for a Single Client (Id 21)
+SELECT * 
+FROM Intervention i
+INNER JOIN Client c
+ON i.ClientId = c.ClientId
+LEFT JOIN QualityReport ql
+ON i.InterventionId = ql.InterventionId
+WHERE c.ClientId = 21;
+
