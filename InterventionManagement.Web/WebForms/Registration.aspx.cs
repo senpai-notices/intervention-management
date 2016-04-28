b971ea1b7081cfba41c8f5eba5c508c3dab141cd
a-mackay <11742400@outlook.com>
2016-04-28 13:09:21 +1000

    Switch Registration page to use new Identity models in business layer

--- InterventionManagement.Web/WebForms/Registration.aspx.cs
+++ InterventionManagement.Web/WebForms/Registration.aspx.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity.Owin;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Identity;
 
 namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.WebForms
 {
@@ -21,26 +22,29 @@ namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.WebForms
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
-            var user = new ApplicationUser() { UserName = txtUsername.Text };
-            IdentityResult result = manager.Create(user, txtPassword.Text);
-            if (result.Succeeded)
-            {
-                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                //string code = manager.GenerateEmailConfirmationToken(user.Id);
-                //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
-                //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+            //var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            //var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
+            //var user = new ApplicationUser() { UserName = txtUsername.Text };
+            //IdentityResult result = manager.Create(user, txtPassword.Text);
+            //if (result.Succeeded)
+            //{
+            //    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+            //    //string code = manager.GenerateEmailConfirmationToken(user.Id);
+            //    //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
+            //    //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
 
-                signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
-                //IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                Response.Redirect("/Webforms/Add_New_Client.aspx");
-            }
-            else
-            {
-                Label5.Text = result.Errors.FirstOrDefault();
-            }
-            
+            //    signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+            //    //IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            //    Response.Redirect("/Webforms/Add_New_Client.aspx");
+            //}
+            //else
+            //{
+            //    Label5.Text = result.Errors.FirstOrDefault();
+            //}
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string roleName = "Engineer";//remove hardcode
+            new IdentityWrapper().CreateUser(username, password, roleName);
         }
     }
 }
