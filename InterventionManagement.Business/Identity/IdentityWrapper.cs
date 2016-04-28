d376d287c8a3f1e9af90e3ce9e589d47ebba0381
a-mackay <11742400@outlook.com>
2016-04-28 12:11:59 +1000

    Add Identity and Entity Framework to business project

---
+++ InterventionManagement.Business/Identity/IdentityWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Identity
+{
+    public class IdentityWrapper
+    {
+        // encapsulates Identity functions
+        public void CreateUser()
+        {
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var myinfo = new MyUserInfo() { FirstName = "Pranav", LastName = "Rastogi" };
+            string name = "Admin";
+            string password = "123456";
+            string test = "test";
+
+            //Create Role Test and User Test
+            RoleManager.Create(new IdentityRole(test));
+            UserManager.Create(new ApplicationUser() { UserName = test });
+
+            //Create Role Admin if it does not exist
+            if (!RoleManager.RoleExists(name))
+            {
+                var roleresult = RoleManager.Create(new IdentityRole(name));
+            }
+        }
+    }
+}
