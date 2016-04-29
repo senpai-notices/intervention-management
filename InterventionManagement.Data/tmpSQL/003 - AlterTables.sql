ff94c4fbc31b0803cf584845d0af3c19f2dd0792
a-mackay <11742400@outlook.com>
2016-04-29 11:38:17 +1000

    Removed temp scripts folder

--- InterventionManagement.Data/tmpSQL/003 - AlterTables.sql
+++
@@ -1,7 +0,0 @@
-﻿DROP TABLE [dbo].[Accountant];
-DROP TABLE [dbo].[Engineer];
-DROP TABLE [dbo].[Manager];
-DROP TABLE [dbo].[QualityReport];
-DROP TABLE [dbo].[User];
-DROP TABLE [dbo].[Intervention];
-DROP TABLE [dbo].[InterventionTemplate];
