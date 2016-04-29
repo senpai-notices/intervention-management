b554656ce61a5d8c8911bc7ac7ac738b57556e97
a-mackay <11742400@outlook.com>
2016-04-29 10:31:34 +1000

    Allow more characters for District name

--- InterventionManagement.Data/SqlScripts/Script0001-CreateTables.sql
+++ InterventionManagement.Data/SqlScripts/Script0001-CreateTables.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[District]
 (
 	[DistrictId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(50) NOT NULL
+    [Name] VARCHAR(100) NOT NULL
 )
 
 CREATE TABLE [dbo].[User]
