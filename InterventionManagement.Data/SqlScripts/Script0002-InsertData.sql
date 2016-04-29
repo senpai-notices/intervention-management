37ce29ab6135c654e96bbdce848ed7f79fce6a98
a-mackay <11742400@outlook.com>
2016-04-29 11:14:08 +1000

    Remove QMI Inserts

--- InterventionManagement.Data/SqlScripts/Script0002-InsertData.sql
+++ InterventionManagement.Data/SqlScripts/Script0002-InsertData.sql
@@ -14,13 +14,6 @@ INSERT INTO [dbo].[InterventionState] ([InterventionStateId], [InterventionState
 INSERT INTO [dbo].[InterventionState] ([InterventionStateId], [InterventionStateName]) VALUES (4, N'Cancelled')
 SET IDENTITY_INSERT [dbo].[InterventionState] OFF
 
-INSERT INTO [dbo].[Manager] ([ManagerId], [HoursApprovalLimit], [CostApprovalLimit], [DistrictId]) VALUES (4, CAST(200 AS Decimal(18, 0)), CAST(50000 AS Decimal(18, 0)), 4)
-INSERT INTO [dbo].[Manager] ([ManagerId], [HoursApprovalLimit], [CostApprovalLimit], [DistrictId]) VALUES (5, CAST(1000 AS Decimal(18, 0)), CAST(200000 AS Decimal(18, 0)), 5)
-
-INSERT INTO [dbo].[Engineer] ([EngineerId], [HoursApprovalLimit], [CostApprovalLimit], [DistrictId]) VALUES (1, CAST(50 AS Decimal(18, 0)), CAST(2000 AS Decimal(18, 0)), 4)
-INSERT INTO [dbo].[Engineer] ([EngineerId], [HoursApprovalLimit], [CostApprovalLimit], [DistrictId]) VALUES (2, CAST(100 AS Decimal(18, 0)), CAST(5200 AS Decimal(18, 0)), 4)
-INSERT INTO [dbo].[Engineer] ([EngineerId], [HoursApprovalLimit], [CostApprovalLimit], [DistrictId]) VALUES (3, CAST(10 AS Decimal(18, 0)), CAST(10000 AS Decimal(18, 0)), 5)
-
 SET IDENTITY_INSERT [dbo].[InterventionTemplate] ON
 INSERT INTO [dbo].[InterventionTemplate] ([InterventionTemplateId], [Name], [Hours], [Cost]) VALUES (2, N'Supply and Install Portable Toilet', CAST(2 AS Decimal(18, 0)), CAST(600 AS Decimal(18, 0)))
 INSERT INTO [dbo].[InterventionTemplate] ([InterventionTemplateId], [Name], [Hours], [Cost]) VALUES (3, N'Hepatitis Avoidance Training', CAST(3 AS Decimal(18, 0)), CAST(0 AS Decimal(18, 0)))
@@ -31,19 +24,12 @@ INSERT INTO [dbo].[InterventionTemplate] ([InterventionTemplateId], [Name], [Hou
 INSERT INTO [dbo].[InterventionTemplate] ([InterventionTemplateId], [Name], [Hours], [Cost]) VALUES (8, N'Prepare Sewerage Trench', CAST(50 AS Decimal(18, 0)), CAST(0 AS Decimal(18, 0)))
 SET IDENTITY_INSERT [dbo].[InterventionTemplate] OFF
 
-INSERT INTO [dbo].[Client] ([ClientId], [Name], [Location], [DistrictId]) VALUES (21, N'The Client', N'24 Main St, blue house', 4)
-INSERT INTO [dbo].[Client] ([ClientId], [Name], [Location], [DistrictId]) VALUES (22, N'Community A', N'24 Big Rd, black house', 4)
-INSERT INTO [dbo].[Client] ([ClientId], [Name], [Location], [DistrictId]) VALUES (23, N'UTS Students', N'Broadway, brown building', 5)
+INSERT INTO [dbo].[Client] ([ClientId], [Name], [Location], [DistrictId]) VALUES (1, N'Jane Doe', N'1 Main Street, (Blue House)', 6)
+INSERT INTO [dbo].[Client] ([ClientId], [Name], [Location], [DistrictId]) VALUES (2, N'Josiah and Ruth Family', N'1 Tropical Street', 3)
+INSERT INTO [dbo].[Client] ([ClientId], [Name], [Location], [DistrictId]) VALUES (3, N'David Bowie', N'Cloud 1, Heaven', 5)
 
 SET IDENTITY_INSERT [dbo].[Intervention] ON
 INSERT INTO [dbo].[Intervention] ([InterventionId], [DatePerformed], [InterventionStateId], [Name], [Hours], [Cost], [ProposerId], [ApproverId], [ClientId]) VALUES (7, N'2016-02-15', 1, N'Supply Mosquito Net', CAST(0 AS Decimal(18, 0)), CAST(25 AS Decimal(18, 0)), 1, NULL, 21)
 INSERT INTO [dbo].[Intervention] ([InterventionId], [DatePerformed], [InterventionStateId], [Name], [Hours], [Cost], [ProposerId], [ApproverId], [ClientId]) VALUES (8, N'2016-02-16', 1, N'Prepare Sewerage Trench', CAST(50 AS Decimal(18, 0)), CAST(0 AS Decimal(18, 0)), 1, NULL, 21)
 INSERT INTO [dbo].[Intervention] ([InterventionId], [DatePerformed], [InterventionStateId], [Name], [Hours], [Cost], [ProposerId], [ApproverId], [ClientId]) VALUES (9, N'2016-02-17', 1, N'Hepatitis Avoidance Training', CAST(3 AS Decimal(18, 0)), CAST(0 AS Decimal(18, 0)), 2, NULL, 23)
 SET IDENTITY_INSERT [dbo].[Intervention] OFF
-
-SET IDENTITY_INSERT [dbo].[QualityReport] ON
-INSERT INTO [dbo].[QualityReport] ([QualityReportId], [DateAdded], [Notes], [EffectiveLife], [InterventionId]) VALUES (1, N'2016-04-01', N'Notes!', CAST(90 AS Decimal(18, 0)), 7)
-INSERT INTO [dbo].[QualityReport] ([QualityReportId], [DateAdded], [Notes], [EffectiveLife], [InterventionId]) VALUES (2, N'2016-04-02', N'Some notes!', CAST(75 AS Decimal(18, 0)), 8)
-INSERT INTO [dbo].[QualityReport] ([QualityReportId], [DateAdded], [Notes], [EffectiveLife], [InterventionId]) VALUES (3, N'2016-04-03', N'Depreciation', CAST(60 AS Decimal(18, 0)), 7)
-INSERT INTO [dbo].[QualityReport] ([QualityReportId], [DateAdded], [Notes], [EffectiveLife], [InterventionId]) VALUES (5, N'2016-04-04', N'Important notes', CAST(28 AS Decimal(18, 0)), 9)
-SET IDENTITY_INSERT [dbo].[QualityReport] OFF
