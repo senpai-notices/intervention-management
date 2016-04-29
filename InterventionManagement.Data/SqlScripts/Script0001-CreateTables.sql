43bea0ffcb200edc03e5d96b38b128d218ef497c
a-mackay <11742400@outlook.com>
2016-04-29 11:09:03 +1000

    Merge QMI information back into the Intervention table

--- InterventionManagement.Data/SqlScripts/Script0001-CreateTables.sql
+++ InterventionManagement.Data/SqlScripts/Script0001-CreateTables.sql
@@ -6,7 +6,7 @@
 
 CREATE TABLE [dbo].[User]
 (
-    [Username] VARCHAR(40) NOT NULL PRIMARY KEY IDENTITY, 
+    [Username] VARCHAR(40) NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(100) NOT NULL 
 )
 
@@ -39,43 +39,36 @@ CREATE TABLE [dbo].[InterventionState]
 CREATE TABLE [dbo].[InterventionTemplate]
 (
 	[InterventionTemplateId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Hours] INT NOT NULL, 
-    [Cost] INT NOT NULL
+    [Name] VARCHAR(150) NOT NULL, 
+    [EstimatedHours] INT NOT NULL, 
+    [EstimatedCost] INT NOT NULL
 )
 
 CREATE TABLE [dbo].[Client]
 (
-	[ClientId] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Location] VARCHAR(100) NOT NULL, 
+	[ClientId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(100) NOT NULL, 
+    [Location] VARCHAR(150) NOT NULL, 
     [DistrictId] INT NOT NULL, 
     CONSTRAINT [FK_Client_District] FOREIGN KEY ([DistrictId]) REFERENCES [District]([DistrictId])
 )
 
-CREATE TABLE [dbo].[Intervention] (
-    [InterventionId]      INT          IDENTITY (1, 1) NOT NULL,
-    [DatePerformed]       DATE         NOT NULL,
-    [InterventionStateId] INT          NOT NULL,
-    [Name]                VARCHAR (50) NOT NULL,
-    [Hours]               INT          NOT NULL,
-    [Cost]                INT          NOT NULL,
-    [ProposerId]          INT          NOT NULL,
-    [ApproverId]          INT          NULL,
-    [ClientId]            INT          NOT NULL,
-    PRIMARY KEY CLUSTERED ([InterventionId] ASC),
+CREATE TABLE [dbo].[Intervention]
+(
+    [InterventionId] INT NOT NULL PRIMARY KEY IDENTITY,
+	[InterventionTemplateId] INT NOT NULL,
+    [DatePerformed] DATE NOT NULL,
+    [InterventionStateId] INT NOT NULL,
+    [Hours] INT NOT NULL,
+    [Cost] INT NOT NULL,
+    [ProposerId] VARCHAR(40) NOT NULL,
+    [ApproverId] VARCHAR(40) NULL,
+    [ClientId] INT NOT NULL,
+	[Notes] VARCHAR(2000) NULL,
+	[RemainingLife] INT NOT NULL,
+	[DateOfLastVisit] DATE NULL,
     CONSTRAINT [FK_Intervention_InterventionState] FOREIGN KEY ([InterventionStateId]) REFERENCES [dbo].[InterventionState] ([InterventionStateId]),
-    CONSTRAINT [FK_Intervention_UserProposer] FOREIGN KEY ([ProposerId]) REFERENCES [dbo].[User] ([UserId]),
-    CONSTRAINT [FK_Intervention_UserApprover] FOREIGN KEY ([ApproverId]) REFERENCES [dbo].[User] ([UserId]),
+    CONSTRAINT [FK_Intervention_Proposer] FOREIGN KEY ([ProposerId]) REFERENCES [dbo].[Engineer] ([Username]),
+    CONSTRAINT [FK_Intervention_Approver] FOREIGN KEY ([ApproverId]) REFERENCES [dbo].[Manager] ([Username]),
     CONSTRAINT [FK_Intervention_Client] FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Client] ([ClientId])
 );
-
-CREATE TABLE [dbo].[QualityReport]
-(
-	[QualityReportId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [DateAdded] DATE NOT NULL, 
-    [Notes] VARCHAR(100) NOT NULL, 
-    [EffectiveLife] INT NOT NULL, 
-    [InterventionId] INT NOT NULL, 
-    CONSTRAINT [FK_QualityReport_Intervention] FOREIGN KEY (InterventionId) REFERENCES [Intervention](InterventionId)
-)
