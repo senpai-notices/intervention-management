fd85f0dd80b6527c1adcb9f27d998aae56b4c9c1
Alex Tan <alext@outlook.com.au>
2016-05-03 18:54:25 +1000

    Transactionise proc

--- InterventionManagement.Data/SqlScripts/Script0003-StoredProcs.sql
+++ InterventionManagement.Data/SqlScripts/Script0003-StoredProcs.sql
@@ -3,3 +3,25 @@
 AS
 	SELECT * FROM [dbo].Engineer
 	WHERE EngineerUsername like @username
+GO
+
+CREATE PROCEDURE [dbo].[InsertEngineer]
+	@username varchar(40), @name varchar(100), @hours int, @cost int, @districtId int
+AS
+	BEGIN TRY
+
+		BEGIN TRAN Main
+	
+		INSERT INTO [dbo].[User](Username, Name)
+		VALUES(@username, @name)
+
+		INSERT INTO [dbo].[Engineer](EngineerUsername, CostApprovalLimit, HoursApprovalLimit, DistrictId)
+		VALUES(@username, @hours, @cost, @districtId)
+
+		COMMIT TRAN Main
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN Main
+		PRINT 'InsertEngineer failed.'
+	END CATCH
+GO
