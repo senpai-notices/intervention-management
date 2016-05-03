cf48bb1835f27f17eac0833861e430b9ce82aa5b
Alex Tan <alext@outlook.com.au>
2016-05-03 23:12:02 +1000

    Remove Xunit due to bug

--- InterventionManagement.Tests/EngineerTests.cs
+++ InterventionManagement.Tests/EngineerTests.cs
@@ -1,23 +1,35 @@
 ﻿using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
 {
+    [TestClass]
     public class EngineerTests
     {
-        [Theory]
-        [InlineData("tester", "Tester Smith", 3, 4, 1)]
-        [InlineData("alex", "Alex Smith", 0, 0, 2)]
-        public void Meth1(string username, string name, int hours, int cost, int districtId)
+        [TestMethod]
+        public void InsertEngineer_ValidData_Success()
         {
+            var username = "alex";
+            var name = "Alex";
+            var hours = 3;
+            var cost = 4;
+            var districtId = 5;
+
             var engineerTableWrapper = new EngineerTableWrapper();
             engineerTableWrapper.InsertEngineer(username, name, hours, cost, districtId);
 
             var engineerDataTable = engineerTableWrapper.GetEngineerByEngineerUsername(username);
 
-            Assert.Equal(engineerDataTable.FindByEngineerUsername(username).EngineerUsername, username);
+            Assert.AreEqual(engineerDataTable.FindByEngineerUsername(username).EngineerUsername, username);
 
             engineerTableWrapper.DeleteEngineer(username);
         }
+
+        public void InsertEngineers_ValidData_Success() { }
+
+        public void InsertEngineer_InvalidData_Exception()
+        { }
+
+        public void InsertEngineer_DuplicateUsername_Exception() { }
     }
 }
