9b42afd184ddfb04a48ea109b4fd292ac6b54296
a-mackay <11742400@outlook.com>
2016-05-03 12:27:25 +1000

    Add engy business class

---
+++ InterventionManagement.Business/DataLayerWrappers/ClientTableWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers
+{
+    class ClientTableWrapper
+    {
+        public ClientTableWrapper()
+        {
+
+        }
+    }
+}
---
+++ InterventionManagement.Business/DataLayerWrappers/EngineerTableWrapper.cs
@@ -0,0 +1,23 @@
+﻿using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data.DataSets;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data.DataSets.MainDataSetTableAdapters;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers
+{
+    class EngineerTableWrapper
+    {
+        public void addEngineer(string username, int hoursApprovalLimit, int costApprovalLimit, int districtId)
+        {
+            MainDataSet.EngineerDataTable engineers = new EngineerTableAdapter().GetData();
+            MainDataSet.EngineerRow newEngineer = engineers.NewEngineerRow();
+
+            newEngineer.EngineerUsername = username;
+            newEngineer.HoursApprovalLimit = hoursApprovalLimit;
+            newEngineer.CostApprovalLimit = costApprovalLimit;
+            newEngineer.DistrictId = districtId;
+
+            engineers.Rows.Add(newEngineer);
+
+            new EngineerTableAdapter().Update(engineers);
+        }
+    }
+}
