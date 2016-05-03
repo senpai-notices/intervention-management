5be187d80adff34f830ea7e70a2afa880cc89d1a
a-mackay <11742400@outlook.com>
2016-05-03 18:02:25 +1000

    Delete demo methods for data layer access from Add_New_Client page

--- InterventionManagement.Web/WebForms/Add_New_Client.aspx.cs
+++ InterventionManagement.Web/WebForms/Add_New_Client.aspx.cs
@@ -22,39 +22,6 @@ namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.WebForms
         {
             bool validName = !String.IsNullOrWhiteSpace(txt_Name.Text);
 
-            // showing that we can access data from typed datasets
-            MainDataSet.ClientDataTable clients = new ClientTableAdapter().GetData();
-            foreach (MainDataSet.ClientRow clientRow in clients)
-            {
-                Response.Write("<script>alert('" + clientRow.Name + clientRow.Location + "')</script>");
-            }
-
-            MainDataSet.ClientRow testRow = clients.NewClientRow();
-            testRow.Location = "test location";
-            testRow.Name = "John Smithy";
-            testRow.DistrictId = 5;
-            //testRow.ClientId = 10;// dont need this line, autogenerates Ids
-
-            clients.Rows.Add(testRow);
-            Response.Write("<script>alert('" + "new row added" + "')</script>");
-            foreach (MainDataSet.ClientRow clientRow in clients)
-            {
-                Response.Write("<script>alert('" + clientRow.Name + clientRow.Location + "')</script>");
-            }
-            
-            ClientTableAdapter cta = new ClientTableAdapter();
-            cta.Update(clients);
-            // end the demo section
-            // testing FillByClientId
-            Response.Write("<script>alert('" + "testing fillbyclientid method" + "')</script>");
-
-            //clients = new ClientTableAdapter().GetDataByClientId(10);
-            foreach (MainDataSet.ClientRow clientRow in clients)
-            {
-                Response.Write("<script>alert('" + clientRow.Name + clientRow.Location + "')</script>");
-            }
-            // end test 2
-
             if (validName)
             {
                 string message = "New Client Created. Name = " + txt_Name.Text + " District = " + dropList_District.SelectedItem.Text;
