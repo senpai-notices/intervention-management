1765bfc2c1d7a9fd4df1d86b89c5d55f84bcf571
Alex Tan <alext@outlook.com.au>
2016-05-03 18:13:53 +1000

    sample test file

---
+++ InterventionManagement.Tests/NewTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    [TestClass]
+    public class NewTests
+    {
+        [TestMethod]
+        public void Meth1()
+        {
+
+            
+        }
+    }
+}
