6478b9713ed7c290531b0dade14e60d57c021b64
John Fox <J.Arcoudis@gmail.com>
2016-05-03 20:31:49 +1000

    Add "Update Intervention Life"

--- InterventionManagement.Business/Services/InterventionManager.cs
+++ InterventionManagement.Business/Services/InterventionManager.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Helpers;
+using System;
 
 namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Services
 {
@@ -16,6 +17,20 @@ namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Services
             bool approvalRequired = InterventionValidator.RequiresApproval(engineerUsername, InterventionTypeID);
             
             // _intervention.AddNewIntervention(int InterventionTypeID, int cost, int hours, int clientID, string engineerUsername, approvalRequired);            
+        }
+
+        public static bool UpdateInterventionLife(int InterventionID, int remainingLife, string notes)
+        {
+            // return False if invalid inputs
+
+            if (remainingLife > 100 || remainingLife < 0) return false;
+            if (notes.Length > 2000) return false;
+
+            DateTime today = DateTime.Today;
+
+           // _intervention.UpdateIntervention(InterventionID, remainingLife, notes, today);
+
+            return true;
         }
 
     }
