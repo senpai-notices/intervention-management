a7c4f4aeec86aea8574d83b937e577166525270b
Alex Tan <alext@outlook.com.au>
2016-05-03 21:15:38 +1000

    Add new tests with XUnit package

---
+++ InterventionManagement.Tests/EngineerTests.cs
@@ -0,0 +1,23 @@
+﻿using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers;
+using Xunit;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Tests
+{
+    public class EngineerTests
+    {
+        [Theory]
+        [InlineData("tester", "Tester Smith", 3, 4, 1)]
+        [InlineData("alex", "Alex Smith", 0, 0, 2)]
+        public void Meth1(string username, string name, int hours, int cost, int districtId)
+        {
+            var engineerTableWrapper = new EngineerTableWrapper();
+            engineerTableWrapper.InsertEngineer(username, name, hours, cost, districtId);
+
+            var engineerDataTable = engineerTableWrapper.GetEngineerByEngineerUsername(username);
+
+            Assert.Equal(engineerDataTable.FindByEngineerUsername(username).EngineerUsername, username);
+
+            engineerTableWrapper.DeleteEngineer(username);
+        }
+    }
+}
