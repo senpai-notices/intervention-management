308975fb6bff1aa664edb45b86de7acb15c27d3b
Alex Tan <alext@outlook.com.au>
2016-05-04 02:30:10 +1000

    Added procs for User

--- InterventionManagement.Data/SqlScripts/Script0003-StoredProcs.sql
+++ InterventionManagement.Data/SqlScripts/Script0003-StoredProcs.sql
@@ -1,4 +1,8 @@
-﻿CREATE PROCEDURE [dbo].[GetEngineerByEngineerUsername]
+﻿/*
+ENGINEER
+*/
+
+CREATE PROCEDURE [dbo].[GetEngineerByEngineerUsername]
 	@username varchar(40)
 AS
 	SELECT * FROM [dbo].Engineer
@@ -44,5 +48,52 @@ AS
 	BEGIN CATCH
 		ROLLBACK TRAN Main
 		PRINT 'DeleteEngineer failed.'
+	END CATCH
+GO
+
+/*
+USER
+*/
+
+CREATE PROCEDURE [dbo].[GetUserByUsername]
+	@username varchar(40)
+AS
+	SELECT * FROM [dbo].[User]
+	WHERE Username like @username
+GO
+
+CREATE PROCEDURE [dbo].[InsertUser]
+	@username varchar(40), @name varchar(100)
+AS
+	BEGIN TRY
+
+		BEGIN TRAN Main
+	
+		INSERT INTO [dbo].[User](Username, Name)
+		VALUES(@username, @name)
+
+		COMMIT TRAN Main
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN Main
+		PRINT 'InsertUser failed.'
+	END CATCH
+GO
+
+CREATE PROCEDURE [dbo].[DeleteUser]
+	@Original_Username varchar(40)
+AS
+	BEGIN TRY
+
+		BEGIN TRAN Main
+	
+		DELETE FROM [dbo].[User]
+			WHERE (Username = @Original_Username)
+
+		COMMIT TRAN Main
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN Main
+		PRINT 'DeleteUser failed.'
 	END CATCH
 GO
