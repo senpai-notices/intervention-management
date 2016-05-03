5b7ba66728baf392ff23df5c1c7b856faf9f2d24
a-mackay <11742400@outlook.com>
2016-05-03 14:19:16 +1000

    Removed usings

--- InterventionManagement.Web/WebForms/Registration.aspx.cs
+++ InterventionManagement.Web/WebForms/Registration.aspx.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
-using au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.Models;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.Owin.Security;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Web.Identity;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers;
 
