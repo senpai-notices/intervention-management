ef8fa5305045c5168bb4ed52df5862592e332036
Alex Tan <alext@outlook.com.au>
2016-05-04 02:31:06 +1000

    Added basic CRUD

--- InterventionManagement.Business/DataLayerWrappers/UserTableWrapper.cs
+++ InterventionManagement.Business/DataLayerWrappers/UserTableWrapper.cs
@@ -1,10 +1,13 @@
-﻿using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data.DataSets;
+﻿using System;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data.DataSets;
 using au.edu.uts.ASDF.ENETCare.InterventionManagement.Data.DataSets.MainDataSetTableAdapters;
 
 namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers
 {
     public class UserTableWrapper
     {
+        #region DEPRECATED
+        [Obsolete()]
         public void addUser(string username, string name)
         {
             // add a new user
@@ -18,6 +21,38 @@ namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrap
 
             // update the database
             new UserTableAdapter().Update(users);
+        }
+        #endregion
+
+        public MainDataSet.UserDataTable GetUsers()
+        {
+            return new UserTableAdapter().GetData();
+        }
+        
+        public MainDataSet.UserDataTable GetUserByUsername(string username)
+        {
+            return new UserTableAdapter().GetDataBy_Username(username);
+        }
+
+        public void InsertUser(string username, string name)
+        {
+            new UserTableAdapter().InsertUser(username, name);
+
+            var dataTable = GetUserByUsername(username);
+
+            if (dataTable.Rows.Count == 0)
+            {
+                new UserTableAdapter().InsertUser(username, name);
+            }
+            else
+            {
+                throw new Exception("Duplicate username.");
+            }
+        }
+
+        public void DeleteUser(string username)
+        {
+            new UserTableAdapter().DeleteUser(username);
         }
     }
 }
