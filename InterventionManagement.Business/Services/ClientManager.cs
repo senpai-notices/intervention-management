8441534b5a6b110075100761e2a71e6f8fdf3e04
John Fox <J.Arcoudis@gmail.com>
2016-05-04 10:39:57 +1000

    Created Intervention/Clint Manager

---
+++ InterventionManagement.Business/Services/ClientManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Helpers;
+using System;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Services
+{
+    // Should this be static? Static classes cannot implement interfaces
+    public static class ClientManager
+    {
+        private static ClientTableWrapper _clients = new ClientTableWrapper();
+
+        public static bool AddNewClient(string name, string location, int districtID, int engineerDistrict)
+        {
+            try
+            {
+
+                if (ClientValidator.ValidateName(name) && ClientValidator.ValidateLocation(location) && districtID == engineerDistrict)
+                {
+                    //TODO: AddNewClient(Stub)
+                    //Valid Client Entry
+                    //_clients.AddNewClient(name, location, districtID
+                }
+            }
+            catch (System.Exception e)
+            {
+                //Invalid Client Data
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            //Valid Client has beed added
+            return true;
+
+
+        }
+
+        //TODO: GetClientByID (Required Get Client In Wrapper)
+        //public static Client GetClientById(int clientId)
+        //{
+        //    return _clients.FirstOrDefault(c => c.ClientId == clientId);
+        //}
+    }
+}
---
+++ InterventionManagement.Business/Services/InterventionManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.DataLayerWrappers;
+using au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Helpers;
+using System;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Services
+{
+    public static class InterventionManager
+    {
+        //private static InterventionTableWrapper _intervention = new InterventionTableWrapper();
+
+
+        public static void AddNewIntervention(int InterventionTypeID, int cost, int hours, int clientID, string username)
+        {
+            //TODO: Finish AddNewIntervention.
+
+            if (cost >= 0 && hours >= 0)
+            {
+                // TODO: Add State to new Intervention
+                // _intervention.AddNewIntervention(int InterventionTypeID, int cost, int hours, int clientID, string engineerUsername);
+            }
+
+        }
+
+        public static bool UpdateInterventionLife(int InterventionID, int remainingLife, string notes)
+        {
+            // return False if invalid inputs
+
+            if (remainingLife > 100 || remainingLife < 0) return false;
+            if (notes.Length > 2000) return false;
+
+            DateTime today = DateTime.Today;
+
+            // _intervention.UpdateIntervention(InterventionID, remainingLife, notes, today);
+
+            return true;
+        }
+
+        public static void ApproveIntervention(int InterventionID, string username, string userRole)
+        {
+            // Users from same district can interact with an Interventions. (Webforms ensures this)
+            // 
+            if (userRole.Equals("Manager") || InterventionValidator.VerifyProposerUsername(InterventionID, username))
+            {
+                if (InterventionValidator.CanApprove(InterventionID, username, userRole))
+                {
+
+                }
+                // _intervention.ApproveInterventionByID(InterventionID, DateTime.Today);
+            }
+
+        }
+
+    }
+}
