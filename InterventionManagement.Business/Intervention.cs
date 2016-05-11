3f9712ce0a901943f280a919f229798c11405ba0
a-mackay <11742400@outlook.com>
2016-05-11 11:08:05 +1000

    Change cost data type to Decimal

---
+++ InterventionManagement.Business/Intervention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ASDF.ENETCare.InterventionManagement.Business
+{
+    public class Intervention
+    {
+        public int InterventionID { get; set; }
+
+        public DateTime DatePerformed { get; set; }
+        public int Hours { get; set; }
+        public decimal Cost { get; set; }
+
+        public string Notes { get; set; }
+        public int RemainingLife { get; set; }
+        public DateTime DateOfLastVisit { get; set; }
+
+        public int InterventionTemplateID { get; set; }
+        public int InterventionStateID { get; set; }
+        public int ClientID { get; set; }
+        public int EngineerID { get; set; }
+        public int ManagerID { get; set; }
+    }
+}
