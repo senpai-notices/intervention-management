616f1d59ec1c206cd9487609ea423fd603fbb753
a-mackay <11742400@outlook.com>
2016-05-13 14:39:39 +1000

    Seed Intervention Templates table

--- InterventionManagement.Data/DatabaseInitializer.cs
+++ InterventionManagement.Data/DatabaseInitializer.cs
@@ -7,19 +7,33 @@ namespace ASDF.ENETCare.InterventionManagement.Data
     {
         protected override void Seed(MainContext context)
         {
-            SeedInterventionState(context);
+            SeedInterventionStates(context);
+            SeedInterventionTemplates(context);
         }
 
-        private void SeedInterventionState(MainContext context)
+        private void SeedInterventionStates(MainContext context)
         {
             var interventionStates = new List<InterventionState>
             {
-                new InterventionState{ Name="Proposed"},
-                new InterventionState{ Name="Approved"},
-                new InterventionState{ Name="Cancelled"},
-                new InterventionState{ Name="Completed"}
+                new InterventionState{ Name="Proposed" },
+                new InterventionState{ Name="Approved" },
+                new InterventionState{ Name="Cancelled" },
+                new InterventionState{ Name="Completed" }
             };
             interventionStates.ForEach(s => context.InterventionState.Add(s));
+            context.SaveChanges();
+        }
+
+        private void SeedInterventionTemplates(MainContext context)
+        {
+            var interventionTemplates = new List<InterventionTemplate>
+            {
+                new InterventionTemplate{ Name="Supply Mosquito Net", Hours=1, Cost=60.00M },
+                new InterventionTemplate{ Name="Supply and Install Storm-proof Home Kit", Hours=40, Cost=1000.00M },
+                new InterventionTemplate{ Name="Supply and Install Portable Toilet", Hours=4, Cost=150.00M },
+                new InterventionTemplate{ Name="Hepatitis Avoidance Training", Hours=3, Cost=80.00M }
+            };
+            interventionTemplates.ForEach(s => context.InterventionTemplate.Add(s));
             context.SaveChanges();
         }
     }
