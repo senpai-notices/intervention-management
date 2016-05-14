ebc48c5ab9ea12a23f54ce0e7d15698d1d1401da
Alex Tan <alext@outlook.com.au>
2016-05-15 00:54:31 +1000

    Add Client FK to Intervention

--- InterventionManagement.Business/Intervention.cs
+++ InterventionManagement.Business/Intervention.cs
@@ -12,10 +12,20 @@ namespace ASDF.ENETCare.InterventionManagement.Business
         public int RemainingLife { get; set; }
         public DateTime DateOfLastVisit { get; set; }
         public InterventionState InterventionState { get; set; }
-        
-        // foreign keys
         public int ClientId { get; set; }
-        public int EngineerId { get; set; }
-        public int ManagerId { get; set; }
+
+        public int ProposerId { get; set; }
+        public int ApproverId { get; set; }
+
+        public virtual Client Client { get; set; }
+        // Because Proposer and Approver would be from the same table, we need to do something to disambiguiate it.
+        // http://stackoverflow.com/questions/28570916/defining-multiple-foreign-key-for-the-same-table-in-entity-framework-code-first
+
+        // [ForeignKey("ProposerId")]
+        // [InverseProperty("ProposedInterventions")]
+        // public virtual AppUser Proposer { get; set; }
+        // [ForeignKey("ApproverId")]
+        // [InverseProperty("ApprovedInterventions")]
+        // public virtual AppUser Approver { get; set; }
     }
 }
