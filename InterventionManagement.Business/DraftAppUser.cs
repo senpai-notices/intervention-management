79936c58ab5ced48a65a9e1f8768c70076fb61cf
Alex Tan <alext@outlook.com.au>
2016-05-15 02:38:24 +1000

    Add DraftAppUser

---
+++ InterventionManagement.Business/DraftAppUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace ASDF.ENETCare.InterventionManagement.Business
+{
+    public class DraftAppUser
+    {
+        public int DraftAppUserId { get; set; }
+        public string Username { get; set; }
+        public DraftAppUserRole AppUserRole { get; set; }
+
+        public virtual ICollection<Intervention> ProposedInterventions { get; set; }
+        public virtual ICollection<Intervention> ApprovedInterventions { get; set; }
+    }
+
+    public enum DraftAppUserRole
+    {
+        Accountant = 1,
+        Manager,
+        Engineer
+    }
+}
