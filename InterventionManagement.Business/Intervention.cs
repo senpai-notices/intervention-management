9f76efa56091c74a32effed30f7efe3dc48bcf0b
Alex Tan <alext@outlook.com.au>
2016-05-15 02:40:09 +1000

    Add temporary name property and DateTime2 annotations

--- InterventionManagement.Business/Intervention.cs
+++ InterventionManagement.Business/Intervention.cs
@@ -1,31 +1,37 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASDF.ENETCare.InterventionManagement.Business
 {
     public class Intervention
     {
         public int InterventionId { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "DateTime2")]
         public DateTime DatePerformed { get; set; }
         public int Hours { get; set; }
         public decimal Cost { get; set; }
         public string Notes { get; set; }
         public int RemainingLife { get; set; }
+        [Column(TypeName = "DateTime2")]
         public DateTime DateOfLastVisit { get; set; }
         public InterventionState InterventionState { get; set; }
         public int ClientId { get; set; }
+        // TODO: add template FK and remove name?
 
         public int ProposerId { get; set; }
         public int ApproverId { get; set; }
 
         public virtual Client Client { get; set; }
-        // Because Proposer and Approver would be from the same table, we need to do something to disambiguiate it.
+
+        // Because Proposer and Approver are from the same table (User), we need to disambiguiate it.
         // http://stackoverflow.com/questions/28570916/defining-multiple-foreign-key-for-the-same-table-in-entity-framework-code-first
 
-        // [ForeignKey("ProposerId")]
-        // [InverseProperty("ProposedInterventions")]
-        // public virtual AppUser Proposer { get; set; }
-        // [ForeignKey("ApproverId")]
-        // [InverseProperty("ApprovedInterventions")]
-        // public virtual AppUser Approver { get; set; }
+/*        [ForeignKey("ProposerId")]
+        [InverseProperty("ProposedInterventions")]*/
+        public virtual DraftAppUser Proposer { get; set; }
+/*        [ForeignKey("ApproverId")]
+        [InverseProperty("ApprovedInterventions")]*/
+        public virtual DraftAppUser Approver { get; set; }
     }
 }
