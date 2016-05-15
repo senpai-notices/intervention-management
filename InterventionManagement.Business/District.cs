c5452931f3221726e8bdc38efd9c1c1e57775cfc
Alex Tan <alext@outlook.com.au>
2016-05-15 15:04:46 +1000

    Add ToString() support to District

--- InterventionManagement.Business/District.cs
+++ InterventionManagement.Business/District.cs
@@ -1,12 +1,20 @@
-﻿namespace ASDF.ENETCare.InterventionManagement.Business
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASDF.ENETCare.InterventionManagement.Business
 {
     public enum District
     {
+        [Display(Name = "Urban Indonesia")]
         UrbanIndonesia = 1,
+        [Display(Name = "Rural Indonesia")]
         RuralIndonesia,
+        [Display(Name = "Urban Papua New Guinea")]
         UrbanPapuaNewGuinea,
+        [Display(Name = "Rural Papua New Guinea")]
         RuralPapuaNewGuinea,
+        [Display(Name = "Sydney")]
         Sydney,
+        [Display(Name = "Rural New South Wales")]
         RuralNewSouthWales
     }
 }
