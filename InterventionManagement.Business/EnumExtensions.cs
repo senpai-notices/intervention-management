4482b452d4050d3bb7bc41f486d89170b7053352
Alex Tan <alext@outlook.com.au>
2016-05-15 15:04:07 +1000

    Add enum ToString() support

---
+++ InterventionManagement.Business/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASDF.ENETCare.InterventionManagement.Business
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        ///     A generic extension method that aids in reflecting 
+        ///     and retrieving any attribute that is applied to an `Enum`.
+        /// </summary>
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+                where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
+    }
+}
