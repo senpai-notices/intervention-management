a145c75cb28f7f49c1839b76d896960feb908d6d
Alex Tan <alext@outlook.com.au>
2016-05-15 15:18:10 +1000

    GetDisplayName() now GetName()

--- InterventionManagement.Business/EnumExtensions.cs
+++ InterventionManagement.Business/EnumExtensions.cs
@@ -11,7 +11,7 @@ namespace ASDF.ENETCare.InterventionManagement.Business
         ///     A generic extension method that aids in reflecting 
         ///     and retrieving any attribute that is applied to an `Enum`.
         /// </summary>
-        public static string GetDisplayName(this Enum enumValue)
+        public static string GetName(this Enum enumValue)
         {
             return enumValue.GetType()
                             .GetMember(enumValue.ToString())
