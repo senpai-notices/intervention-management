649dd47e928a7487321c1774d5b6d853ffda3d96
a-mackay <11742400@outlook.com>
2016-05-16 21:25:37 +1000

    Fix connection strings to point to same database

--- InterventionManagement.Web/Global.asax.cs
+++ InterventionManagement.Web/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using ASDF.ENETCare.InterventionManagement.Data
+using ASDF.ENETCare.InterventionManagement.Data;
 
 
 namespace InterventionManagement.Web
