e39a6de4bbf718f18b54f784fad53ecfcde1dcbc
a-mackay <11742400@outlook.com>
2016-05-16 21:16:44 +1000

    Add SetDataDirectory method and initialize database method

--- InterventionManagement.Web/Global.asax.cs
+++ InterventionManagement.Web/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ASDF.ENETCare.InterventionManagement.Data
+
 
 namespace InterventionManagement.Web
 {
@@ -16,6 +19,22 @@ namespace InterventionManagement.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Additional custom startup methods
+            SetDataDirectory();
+            InitializeDatabase();
+        }
+
+        private void SetDataDirectory()
+        {
+            // Set DataDirectory for the connection string in Web.config
+            string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\InterventionManagement.Data"));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
+
+        private void InitializeDatabase()
+        {
+            ASDF.ENETCare.InterventionManagement.Data.Startup.InitializeDatabase();
         }
     }
 }
