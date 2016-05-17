bbb00b46eb2da591cd70f1cafc69f5ec864e8707
Tuan Vinh Ha <vinh.ha3@hotmail.com>
2016-05-17 16:10:10 +1000

    Added view intervention associated with client

--- InterventionManagement.Web/Controllers/InterventionsController.cs
+++ InterventionManagement.Web/Controllers/InterventionsController.cs
@@ -5,129 +5,118 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
-using System.Web.DynamicData;
 using System.Web.Mvc;
+using System.Web.Routing;
 using ASDF.ENETCare.InterventionManagement.Data;
 
 namespace ASDF.ENETCare.InterventionManagement.Web.Controllers
 {
-    public class EngineerController : Controller
+    public class InterventionsController : Controller
     {
+        
         private MainContext db = new MainContext();
 
-        // GET: Engineer
-        public ActionResult Index()
+        // GET: Interventions
+        public ActionResult Index(int id) //int id has been added to get the clientID
         {
-            
-            var query = from client in db.Client where client.DistrictId == 6 select client; //Assume 6 is the DistrictID of the engineer
+            var query = from intervention in db.Intervention where intervention.ClientId == id select intervention;
 
-            return View(query); //db.Client.ToList() => This will show all clients
+            return View(query.ToList());
         }
 
-        // GET: Engineer/Details/5
+        // GET: Interventions/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Client client = db.Client.Find(id);
-            if (client == null)
+            Intervention intervention = db.Intervention.Find(id);
+            if (intervention == null)
             {
                 return HttpNotFound();
             }
-            return View(client);
+            return View(intervention);
         }
 
-        // GET: Engineer/Create
+        // GET: Interventions/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Engineer/Create
+        // POST: Interventions/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ClientId,Name,Location,DistrictId")] Client client)
+        public ActionResult Create([Bind(Include = "InterventionId,DatePerformed,Hours,Cost,Notes,RemainingLife,DateOfLastVisit,InterventionTemplateId,InterventionStateId,ClientId,EngineerId,ManagerId")] Intervention intervention)
         {
             if (ModelState.IsValid)
             {
-                Client newClient = new Client();
-                newClient.Name = client.Name;
-                newClient.Location = client.Location;
-                newClient.DistrictId = 6;// Assume 6 is the DistrictID of the engineer
-
-                db.Client.Add(newClient);
+                db.Intervention.Add(intervention);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(client);
+            return View(intervention);
         }
 
-        // GET: Engineer/Edit/5
+        // GET: Interventions/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Client client = db.Client.Find(id);
-            if (client == null)
+            Intervention intervention = db.Intervention.Find(id);
+            if (intervention == null)
             {
                 return HttpNotFound();
             }
-            return View(client);
+            return View(intervention);
         }
 
-        // POST: Engineer/Edit/5
+        // POST: Interventions/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ClientId,Name,Location,DistrictId")] Client client)
+        public ActionResult Edit([Bind(Include = "InterventionId,DatePerformed,Hours,Cost,Notes,RemainingLife,DateOfLastVisit,InterventionTemplateId,InterventionStateId,ClientId,EngineerId,ManagerId")] Intervention intervention)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(client).State = EntityState.Modified;
+                db.Entry(intervention).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(client);
+            return View(intervention);
         }
 
-        // GET: Engineer/Delete/5
+        // GET: Interventions/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Client client = db.Client.Find(id);
-            if (client == null)
+            Intervention intervention = db.Intervention.Find(id);
+            if (intervention == null)
             {
                 return HttpNotFound();
             }
-            return View(client);
+            return View(intervention);
         }
 
-        // POST: Engineer/Delete/5
+        // POST: Interventions/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Client client = db.Client.Find(id);
-            db.Client.Remove(client);
+            Intervention intervention = db.Intervention.Find(id);
+            db.Intervention.Remove(intervention);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        public ActionResult ViewInterventions(int id)
-        {
-            return RedirectToAction("Index","Interventions",new {id}); //Move to different page. id is equal to client ID. See Engineer/Details.cshtml
-            
         }
 
         protected override void Dispose(bool disposing)
