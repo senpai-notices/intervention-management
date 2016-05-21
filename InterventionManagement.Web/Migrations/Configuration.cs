c0fe48bf3fcc1d567696c98fe5f17aaa01c9e9ec
mackay <testuhonetuhs@outlook.com>
2016-05-21 15:41:37 +1000

    Add remaining seed methods

--- InterventionManagement.Web/Migrations/Configuration.cs
+++ InterventionManagement.Web/Migrations/Configuration.cs
@@ -33,6 +33,8 @@ namespace ASDF.ENETCare.InterventionManagement.Web.Migrations
             SeedUsers(context);
             SeedDistricts(context);
             SeedClients(context);
+            SeedInterventionStates(context);
+            SeedInterventionTemplates(context);
         }
 
         private void SeedRoles(ApplicationDbContext context)
@@ -54,37 +56,22 @@ namespace ASDF.ENETCare.InterventionManagement.Web.Migrations
 
         private void SeedUsers(ApplicationDbContext context)
         {
-            //if (!context.Users.Any())
-            //{
-            //    var userStore = new UserStore<ApplicationUser>(context);
-            //    var userManager = new UserManager<ApplicationUser>(userStore);
-
-            //    var engineer = new ApplicationUser { UserName = "test@test.com", Email = "test@test.com" };
-            //    var accountant = new ApplicationUser { UserName = "accountant@test.com", Email = "accountant@test.com" };
-            //    var manager = new ApplicationUser { UserName = "manager@test.com", Email = "manager@test.com" };
-            //    string commonPassword = "testTEST12-";
-
-            //    userManager.Create(accountant, commonPassword);
-            //    userManager.AddToRole(engineer.Id, "Accountant");
-
-            //    userManager.Create(engineer, commonPassword);
-            //    userManager.AddToRole(engineer.Id, "Engineer");
-
-            //    userManager.Create(manager, commonPassword);
-            //    userManager.AddToRole(engineer.Id, "Manager");
-            //}
+            // define users
             var hasher = new PasswordHasher();
             string password = hasher.HashPassword("testTEST12-");
+            string securityStamp = "a09sXOsthox90oeE"; // needed to prevent null error http://stackoverflow.com/questions/21918000/mvc5-vs2012-identity-createidentityasync-value-cannot-be-null
 
-            var accountant = new ApplicationUser { UserName = "accountant@test.com", Email = "accountant@test.com" };
-            var engineer = new ApplicationUser { UserName = "test@test.com", Email = "test@test.com" };
-            var manager = new ApplicationUser { UserName = "manager@test.com", Email = "manager@test.com" };
+            var accountant = new ApplicationUser { UserName = "accountant@test.com", Email = "accountant@test.com", PasswordHash = password, SecurityStamp = securityStamp };
+            var engineer = new ApplicationUser { UserName = "test@test.com", Email = "test@test.com", PasswordHash = password, SecurityStamp = securityStamp };
+            var manager = new ApplicationUser { UserName = "manager@test.com", Email = "manager@test.com", PasswordHash = password, SecurityStamp = securityStamp };
 
+            // add them, or update them if necessary
             context.Users.AddOrUpdate(u => u.UserName, engineer, accountant, manager);
-            context.SaveChanges(); // important for adding to roles
+            context.SaveChanges(); // explicity save users, so context.Users.Any() returns true below
 
             if (context.Users.Any())
             {
+                // add users to roles
                 var userStore = new UserStore<ApplicationUser>(context);
                 var userManager = new UserManager<ApplicationUser>(userStore);
                 userManager.AddToRole(accountant.Id, "Accountant");
@@ -114,6 +101,28 @@ namespace ASDF.ENETCare.InterventionManagement.Web.Migrations
                 new Client { Name = "Bambang Bima", Location = "1 Agung Road", DistrictId = 2 },
                 new Client { Name = "Susilo Sinta", Location = "25 Wira Street", DistrictId = 3 },
                 new Client { Name = "Rodney McDonald", Location = "100 Nelson Highway, Wagga Wagga", DistrictId = 4 }
+                );
+        }
+
+        private void SeedInterventionStates(ApplicationDbContext context)
+        {
+            context.InterventionState.AddOrUpdate(
+                i => i.Name,
+                new InterventionState{ InterventionStateId=1, Name="Proposed" },
+                new InterventionState{ InterventionStateId=2, Name="Approved" },
+                new InterventionState{ InterventionStateId=3, Name="Cancelled" },
+                new InterventionState{ InterventionStateId=4, Name="Completed" }
+                );
+        }
+
+        private void SeedInterventionTemplates(ApplicationDbContext context)
+        {
+            context.InterventionTemplate.AddOrUpdate(
+                i => i.Name,  
+                new InterventionTemplate{ InterventionTemplateId=1, Name="Supply Mosquito Net", Hours=1, Cost=60.00M },
+                new InterventionTemplate{ InterventionTemplateId=1, Name="Supply and Install Storm-proof Home Kit", Hours=40, Cost=1000.00M },
+                new InterventionTemplate{ InterventionTemplateId=1, Name="Supply and Install Portable Toilet", Hours=4, Cost=150.00M },
+                new InterventionTemplate{ InterventionTemplateId=1, Name="Hepatitis Avoidance Training", Hours=3, Cost=80.00M }
                 );
         }
     }
