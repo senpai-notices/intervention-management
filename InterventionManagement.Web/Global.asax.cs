232938c1238ed76c1db4fe2e83218d38ddd92eb7
mackay <testuhonetuhs@outlook.com>
2016-05-21 15:44:34 +1000

    Comment out SetDataDirectory for later

--- InterventionManagement.Web/Global.asax.cs
+++ InterventionManagement.Web/Global.asax.cs
@@ -21,37 +21,13 @@ namespace ASDF.ENETCare.InterventionManagement.Web
 
             // Additional custom startup methods
             //SetDataDirectory();
-            InitializeDatabase();
         }
 
-        private void SetDataDirectory()
-        {
-            // Set DataDirectory for the connection string in Web.config
-            string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\InterventionManagement.Data"));
-            AppDomain.CurrentDomain.SetData("DataDirectory", path);
-        }
-
-        private void InitializeDatabase()
-        {
-            //ASDF.ENETCare.InterventionManagement.Data.Startup.InitializeDatabase();
-
-        }
-
-        //public void CreateRoles(List<string> roleNames)
+        //private void SetDataDirectory()
         //{
-        //    var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ApplicationDbContext()));
-        //    foreach (string roleName in roleNames)
-        //    {
-        //        CreateRole(roleName, roleManager);
-        //    }
-        //}
-
-        //public void CreateRole(string roleName, RoleManager<IdentityRole> roleManager)
-        //{
-        //    if (!roleManager.RoleExists(roleName))
-        //    {
-        //        roleManager.Create(new IdentityRole(roleName));
-        //    }
+        //    // Set DataDirectory for the connection string in Web.config
+        //    string path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\InterventionManagement.Data"));
+        //    AppDomain.CurrentDomain.SetData("DataDirectory", path);
         //}
     }
 }
