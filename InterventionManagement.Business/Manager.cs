a127aaa7d28be06c861eab9c3145fa1e966e984a
mackay <testuhonetuhs@outlook.com>
2016-05-21 12:05:21 +1000

    Remove placeholder Engineer, Manager and parent classes

--- InterventionManagement.Business/Manager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ASDF.ENETCare.InterventionManagement.Business
-{
-    public class Manager : AppUser
-    {
-        public virtual ICollection<Intervention> ApprovedInterventions { get; set; }
-    }
-}
