7f45dc138ca17f1e466b0bf2c35cf1cf405d4103
mackay <testuhonetuhs@outlook.com>
2016-05-21 11:53:34 +1000

    Add manager and engineer fields to ApplicationUser

--- InterventionManagement.Web/Models/IdentityModels.cs
+++ InterventionManagement.Web/Models/IdentityModels.cs
@@ -9,6 +9,10 @@ namespace ASDF.ENETCare.InterventionManagement.Web.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int Hours { get; set; }
+        public decimal Cost { get; set; }
+        public int DistrictId { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
