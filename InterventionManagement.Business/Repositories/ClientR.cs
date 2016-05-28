351b956b1852bf2304f3bee1ebd008ae0a140a06
Alex Tan <alext@outlook.com.au>
2016-05-28 11:00:53 +1000

    Add pilot repo

---
+++ InterventionManagement.Business/Repositories/ClientR.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASDF.ENETCare.InterventionManagement.Business.Repositories
+{
+    public class ClientR
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientR(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Client> GetClients()
+        {
+            return _context.Client.ToList();
+        }
+    }
+}
