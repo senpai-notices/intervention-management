02200bcdbd08d3d45db2b47ee6976c7854c1fa34
Alex Tan <alext@outlook.com.au>
2016-05-30 19:31:15 +1000

    Cleanup

--- InterventionManagement.Test/ClientRepositoryTests.cs
+++ InterventionManagement.Test/ClientRepositoryTests.cs
@@ -27,18 +27,16 @@ namespace ASDF.ENETCare.InterventionManagement.Test
 
             Assert.IsTrue(!allClients.Any());
         }
-        //take out selectall() and test getall
+
         [TestMethod]
         public void InsertClient_One_Exists()
         {
             var allClients = new List<Client>();
             _mock.Setup(x => x.Insert(It.IsAny<Client>()))
                 .Callback((Client c) => { allClients.Add(c); });
-            //_mock.Setup(x => x.SelectAll()).Returns(allClients);
 
             _mock.Object.Insert(new Client());
 
-            //Assert.IsTrue(_mock.Object.SelectAll().Count() == 1);
             Assert.IsTrue(allClients.Count == 1);
             _mock.VerifyAll();
         }
@@ -49,12 +47,10 @@ namespace ASDF.ENETCare.InterventionManagement.Test
             var allClients = new List<Client>();
             _mock.Setup(x => x.Insert(It.IsAny<Client>()))
                 .Callback((Client c) => { allClients.Add(c); });
-            //_mock.Setup(x => x.SelectAll()).Returns(allClients);
 
             _mock.Object.Insert(new Client());
             _mock.Object.Insert(new Client());
 
-            //Assert.IsTrue(_mock.Object.SelectAll().Count() == 2);
             Assert.IsTrue(allClients.Count == 2);
             _mock.VerifyAll();
         }
