a481ced1c5838c84511ee9a3153638d221e8a98a
mackay <testuhonetuhs@outlook.com>
2016-05-31 15:14:33 +1000

    Add accountant view models

---
+++ InterventionManagement.Web/Models/AccountantViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASDF.ENETCare.InterventionManagement.Web.Models
+{
+    public class TotalCostByEngineerViewModel
+    {
+        public IEnumerable<TotalCostByEngineerRow> Engineers { get; }
+    }
+
+    public class TotalCostByEngineerRow
+    {
+        public string Name { get; }
+        public int TotalHours { get; }
+        public double TotalCost { get; }
+    }
+}
