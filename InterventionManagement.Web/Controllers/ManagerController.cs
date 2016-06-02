1b748684eba7068598b8e574729a41be794c89a6
Tuan Vinh Ha <vinh.ha3@hotmail.com>
2016-06-02 18:42:45 +1000

    Add manager controller

---
+++ InterventionManagement.Web/Controllers/ManagerController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASDF.ENETCare.InterventionManagement.Business;
+using ASDF.ENETCare.InterventionManagement.Data.Repositories;
+using ASDF.ENETCare.InterventionManagement.Web.Models;
+
+namespace ASDF.ENETCare.InterventionManagement.Web.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private readonly IGenericRepository<Intervention> _interventionRepository;
+        // GET: Manager
+
+        public ManagerController()
+        {
+            _interventionRepository = new GenericRepository<Intervention>(new ApplicationDbContext());
+        }
+
+        public ActionResult Index()
+        {
+            var listViewModel = new InterventionsListViewModel
+            {
+                Interventions = _interventionRepository.SelectAll().Where(x => x.InterventionStateId == 1)
+            };
+            return View(listViewModel);
+        }
+
+        // GET: Manager/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Manager/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manager/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Manager/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Manager/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Manager/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Manager/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
