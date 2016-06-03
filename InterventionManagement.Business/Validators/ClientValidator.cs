e638fbd0802c5142c938d849a5aa15192394950a
John Fox <J.Arcoudis@gmail.com>
2016-06-03 16:04:41 +1000

    Add Client Validator

---
+++ InterventionManagement.Business/Validators/ClientValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace au.edu.uts.ASDF.ENETCare.InterventionManagement.Business.Validatores
+{
+    public static class ClientValidator
+    {
+
+        public static bool ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name) || !name.Any(char.IsLetter))
+                throw new ArgumentException("Please enter a valid name. Name must not contain numbers.");
+
+            // TODO: Extract Name length and place into config/variable folder
+            if (name.Length >= 100)
+                throw new ArgumentException("Please enter a valid name. Name must contain less that 100 characters.");
+
+            return true;
+        }
+
+        public static bool ValidateLocation(string location)
+        {
+            //TODO: Extract StringLength and Place into Config/Variable File
+            if (string.IsNullOrWhiteSpace(location))
+                throw new ArgumentException("Please enter a location description.");
+
+            if (location.Length >= 150)
+                throw new ArgumentException("Please enter a valid Location. Must contain less that 150 characters.");
+
+            return true;
+        }
+    }
+}
