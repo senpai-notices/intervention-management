63581f1c318c5393f7e6fa3c17ec61c881b5eb84
Alex Tan <alext@outlook.com.au>
2016-06-05 13:01:56 +1000

    Set default Name upon registration

--- InterventionManagement.Web/Controllers/AccountController.cs
+++ InterventionManagement.Web/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace ASDF.ENETCare.InterventionManagement.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = "Nameless"};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
