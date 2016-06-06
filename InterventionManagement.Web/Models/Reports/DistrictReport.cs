165fb69cac523bb640972ecf1430f325f372e79d
John Fox <J.Arcoudis@gmail.com>
2016-06-07 02:09:51 +1000

    Add Engineer Reports

---
+++ InterventionManagement.Web/Models/Reports/DistrictReport.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ASDF.ENETCare.InterventionManagement.Business;
+using ASDF.ENETCare.InterventionManagement.Business.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ASDF.ENETCare.InterventionManagement.Web.Models.Reports
+{
+    public class DistrictReport
+    {
+        public Dictionary<ApplicationUser, DistrictReport> EngineersReport { get; set; }
+
+        public void CostHourByEngineerReport(IEnumerable<Intervention> Interventions)
+        {
+            var engineers = new GenericRepository<District>();
+
+            foreach (var district in engineers.GetEngineers())
+            {
+                EngineersReport.Add(district, new DistrictReport());
+            }
+
+            foreach (var intervention in Interventions)
+            {
+                //TODO: Directly reference completed intervention state.
+                if (intervention.InterventionState.InterventionStateId == 3)
+                {
+                    CostHour_DistrictReport report = null;
+                    var responsible = intervention.Approver;
+
+                    if (EngineersReport.TryGetValue(responsible, out report))
+                    {
+                        report.Name = responsible.Name;
+                        report.TotalCost = report.TotalCost + intervention.Cost;
+                        report.TotalHours = report.TotalHours + intervention.Hours;
+                    }
+                }
+            }
+
+        }
+
+        public class CostHour_DistrictReport
+        {
+            [Required]
+            [DisplayName("District Name")]
+            public string Name { get; set; }
+            [Required]
+            [DisplayName("Total Labour Hours")]
+            public int TotalHours { get; set; }
+            [Required]
+            [DisplayName("Total Cost")]
+            public decimal TotalCost { get; set; }
+        }
+    }
+}
---
+++ InterventionManagement.Web/Models/Reports/EngineerReport.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ASDF.ENETCare.InterventionManagement.Business;
+using ASDF.ENETCare.InterventionManagement.Business.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ASDF.ENETCare.InterventionManagement.Web.Models.Reports
+{
+    public class EngineerReport
+    {
+        public Dictionary<ApplicationUser, CostByEngineer> EngineersReport { get; set; }
+
+        public void AverageCostHourByEngineerReport(IEnumerable<Intervention> Interventions)
+        {
+            CostHourByEngineerReport(Interventions);
+
+            //Averages the amounts per engineer
+            foreach (var report in EngineersReport)
+            {
+                int completed = report.Value.Completed;
+                int hours = report.Value.TotalHours;
+                decimal cost = report.Value.TotalCost;
+
+                report.Value.TotalHours = hours/completed;
+                report.Value.TotalCost = cost/completed;
+            }
+        }
+
+        public void CostHourByEngineerReport(IEnumerable<Intervention> Interventions)
+        {
+            EngineerReportsRepository engineers = new EngineerReportsRepository();
+
+            foreach (var engineer in engineers.GetEngineers())
+            {
+                EngineersReport.Add(engineer, new CostByEngineer());
+            }
+
+            foreach (var intervention in Interventions)
+            {
+                //TODO: Directly reference completed intervention state.
+                if (intervention.InterventionState.InterventionStateId == 3)
+                {
+                    CostByEngineer report = null;
+                    var responsible = intervention.Approver;
+
+                    if (EngineersReport.TryGetValue(responsible, out report))
+                    {
+                        report.Completed++;
+                        report.Name = responsible.Name;
+                        report.TotalCost = report.TotalCost + intervention.Cost;
+                        report.TotalHours = report.TotalHours + intervention.Hours;
+                    }
+                }
+            }
+
+        }
+
+        public class CostByEngineer
+        {
+            [Required]
+            [DisplayName("Engineer Name")]
+            public string Name { get; set; }
+            [Required]
+            [DisplayName("Completed Interventions")]
+            public int Completed { get; set; }
+            [Required]
+            [DisplayName("Total Labour Hours")]
+            public int TotalHours { get; set; }
+            [Required]
+            [DisplayName("Total Cost")]
+            public decimal TotalCost { get; set; }
+
+            
+        }
+    }
+}
