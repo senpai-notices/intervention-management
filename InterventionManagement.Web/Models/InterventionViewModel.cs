ae245c16a8634720cb8359bdace2edb9aabe9c7a
Tuan Vinh Ha <vinh.ha3@hotmail.com>
2016-06-08 17:14:02 +1000

    revert changes

--- InterventionManagement.Web/Models/InterventionViewModel.cs
+++ InterventionManagement.Web/Models/InterventionViewModel.cs
@@ -63,10 +63,6 @@ namespace ASDF.ENETCare.InterventionManagement.Web.Models
         [DisplayName("Client")]
         public int ClientId { get; set; }
 
-        [HiddenInput(DisplayValue = false)]
-        public int StateId { get; set; }
-        public string State { get; set; }
-
         [DisplayName("Client")]
         public string ClientName { get; set; }
 
